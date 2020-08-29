5bdf22b339b252e53436591785bff1cdece8dc4b
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-08-29 22:41:51 +0000

    Inical project added

---
+++ ratz-pet-clinic/src/main/java/ratz/springframework/ratzpetclinic/RatzPetClinicApplication.java
@@ -0,0 +1,13 @@
+package ratz.springframework.ratzpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatzPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatzPetClinicApplication.class, args);
+    }
+
+}
