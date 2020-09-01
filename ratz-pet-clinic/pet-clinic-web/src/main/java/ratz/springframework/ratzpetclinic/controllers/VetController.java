aa65411169be14bab7e7e400e6de202bc59a3c1b
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-09-01 00:32:19 +0000

    added vet list and updated controller. closes #15

--- ratz-pet-clinic/pet-clinic-web/src/main/java/ratz/springframework/ratzpetclinic/controllers/VetController.java
+++ ratz-pet-clinic/pet-clinic-web/src/main/java/ratz/springframework/ratzpetclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package ratz.springframework.ratzpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ratz.springframework.ratzpetclinic.services.VetService;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping({"/vets", "/vets/index"})
-    public String listVets(){
+    public String listVets(Model model){
+
+        model.addAttribute("vets" , vetService.findAll());
 
         return "vets/index";
     }
