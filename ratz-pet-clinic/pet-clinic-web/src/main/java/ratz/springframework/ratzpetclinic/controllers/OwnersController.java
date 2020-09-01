fa9da139780a5f4f5162e04ac128095d2a4a4685
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-09-01 00:29:15 +0000

    added owners list and updated controller. closes #16

--- ratz-pet-clinic/pet-clinic-web/src/main/java/ratz/springframework/ratzpetclinic/controllers/OwnersController.java
+++ ratz-pet-clinic/pet-clinic-web/src/main/java/ratz/springframework/ratzpetclinic/controllers/OwnersController.java
@@ -1,14 +1,24 @@
 package ratz.springframework.ratzpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ratz.springframework.ratzpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnersController {
 
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/","/index"})
-     public String listOwners(){
+     public String listOwners(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
 
      return "owners/index";
      }
