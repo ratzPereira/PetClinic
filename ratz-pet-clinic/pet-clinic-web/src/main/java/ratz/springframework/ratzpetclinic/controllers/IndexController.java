d12a233ae87de10c68a14d12a55a9ef8ca4af39e
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-09-02 22:35:55 +0000

    fixed broken links , using "not implemented" view .closes #45

--- ratz-pet-clinic/pet-clinic-web/src/main/java/ratz/springframework/ratzpetclinic/controllers/IndexController.java
+++ ratz-pet-clinic/pet-clinic-web/src/main/java/ratz/springframework/ratzpetclinic/controllers/IndexController.java
@@ -11,5 +11,11 @@ public class IndexController {
     public String index(){
         return "index";
     }
+
+    @RequestMapping({"/oups"})
+    public String oupsHandler(){
+
+        return "notimplemented";
+    }
 }
 
--- ratz-pet-clinic/pet-clinic-web/src/main/java/ratz/springframework/ratzpetclinic/controllers/OwnersController.java
+++ ratz-pet-clinic/pet-clinic-web/src/main/java/ratz/springframework/ratzpetclinic/controllers/OwnersController.java
@@ -22,4 +22,10 @@ public class OwnersController {
 
      return "owners/index";
      }
+
+     @RequestMapping({"/find"})
+    public String findOwners(){
+
+        return "notimplemented";
+     }
 }
