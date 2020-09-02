a8f77987804046d0a238dda446f43afd062255fe
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-09-02 19:07:25 +0000

    added specialities to vets . closes #27

---
+++ ratz-pet-clinic/pet-clinic-data/src/main/java/ratz/springframework/ratzpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package ratz.springframework.ratzpetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
