3f135facbbf1b7dc54115babfce43fb615cb4efc
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-09-02 19:58:11 +0000

    added speciality map service. closes #31

---
+++ ratz-pet-clinic/pet-clinic-data/src/main/java/ratz/springframework/ratzpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,37 @@
+package ratz.springframework.ratzpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import ratz.springframework.ratzpetclinic.model.Speciality;
+import ratz.springframework.ratzpetclinic.services.SpecialitiesService;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Integer> implements SpecialitiesService {
+
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Integer id) {
+        return super.findById(id);
+    }
+}
