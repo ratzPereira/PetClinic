5068126b1efdc6a904b868f409e466aa29447de8
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-09-03 17:20:17 +0000

    Made base entity @mappedsuper class with gerenated IDs

--- ratz-pet-clinic/pet-clinic-data/src/main/java/ratz/springframework/ratzpetclinic/model/BaseEntity.java
+++ ratz-pet-clinic/pet-clinic-data/src/main/java/ratz/springframework/ratzpetclinic/model/BaseEntity.java
@@ -1,9 +1,17 @@
 package ratz.springframework.ratzpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     public Integer getId() {
