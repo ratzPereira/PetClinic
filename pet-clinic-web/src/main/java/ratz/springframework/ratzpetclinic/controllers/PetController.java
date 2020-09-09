3e5550dfc05b63ae0ab882459855096cd3804e96
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-09-09 00:13:18 +0000

    adding model attributes for pets.Closes #56

---
+++ pet-clinic-web/src/main/java/ratz/springframework/ratzpetclinic/controllers/PetController.java
@@ -0,0 +1,44 @@
+package ratz.springframework.ratzpetclinic.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ratz.springframework.ratzpetclinic.model.Owner;
+import ratz.springframework.ratzpetclinic.model.PetType;
+import ratz.springframework.ratzpetclinic.services.OwnerService;
+import ratz.springframework.ratzpetclinic.services.PetTypeService;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+
+    private final PetTypeService petTypeService;
+    private final OwnerService ownerService;
+
+    public PetController(PetTypeService petTypeService, OwnerService ownerService) {
+        this.petTypeService = petTypeService;
+        this.ownerService = ownerService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable("ownerId") Integer ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
+}
