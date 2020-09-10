ab6731235223910d133f4119c8d3b974d79b1348
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-09-10 01:40:54 +0000

    Implement Vets List VIew Closes #62

--- pet-clinic-web/src/main/java/ratz/springframework/ratzpetclinic/controllers/VisitController.java
+++ pet-clinic-web/src/main/java/ratz/springframework/ratzpetclinic/controllers/VisitController.java
@@ -32,9 +32,11 @@ public class VisitController {
 
         dataBinder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
             @Override
-            public void setAsText(String text) throws IllegalArgumentException{
+            public void setAsText(String text) throws IllegalArgumentException {
+
                 setValue(LocalDate.parse(text));
             }
+
         });
     }
 
