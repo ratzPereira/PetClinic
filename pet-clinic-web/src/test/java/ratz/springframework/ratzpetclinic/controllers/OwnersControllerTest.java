45c98c3ca354e6201d3044adc0d752ec6bb58a3a
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2020-09-10 00:40:02 +0000

    just a CircleCi Testing

--- pet-clinic-web/src/test/java/ratz/springframework/ratzpetclinic/controllers/OwnersControllerTest.java
+++ pet-clinic-web/src/test/java/ratz/springframework/ratzpetclinic/controllers/OwnersControllerTest.java
@@ -1,6 +1,5 @@
 package ratz.springframework.ratzpetclinic.controllers;
 
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,32 +13,10 @@ import ratz.springframework.ratzpetclinic.services.OwnerService;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.verifyNoInteractions;
-import static org.mockito.Mockito.when;
-import static org.springframework.data.repository.util.ClassUtils.hasProperty;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.lenient;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.util.*;
-
-import static org.hamcrest.Matchers.*;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 class OwnersControllerTest {
@@ -70,6 +47,7 @@ class OwnersControllerTest {
     }
 
 
+
 //    @Test
 //    void findOwners() throws Exception {
 //
@@ -84,7 +62,7 @@ class OwnersControllerTest {
     @Test
     void displayOwner() throws Exception {
 
-       // when(ownerService.findById(anyInt())).thenReturn(Owner.builder().id(1).build());
+        lenient().when(ownerService.findById(anyInt())).thenReturn(Owner.builder().id(1).build());
         ownerService.findAll();
 
         mockMvc.perform(get("/owners/123"))
